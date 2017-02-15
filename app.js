a00a3fec5224c2a06808e6e5369afc500be3f364
unknown <kim@ZEPHIR-57.zephir.local>
2017-02-15 15:28:24 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,67 @@
+new Vue({
+
+    // We want to target the div with an id of 'events'
+    el: '#location',
+
+    // Here we can register any values or collections that hold data
+    // for the application
+    data: {
+        location: { text: '',name:'',adress:'', description: '', details: '' },
+        locations: [],
+        event: { name: '', description: '', date: '' },
+        events: []
+    },
+
+    // Anything within the ready function will run when the application loads
+    mounted: function() {
+        // When the application loads, we want to call the method that initializes
+        // some data
+    },
+
+    // Methods we want to use in our application are registered here
+    methods: {
+        // We dedicate a method to retrieving and setting some data
+        fetchLocations: function() {
+            var locations = [
+                {
+                    id: 1,
+                    name: 'NANTES MARTYRS',
+                    description: '16 boulevard des Martyrs Nantais de la Résistance (station TOTAL) 44000 NANTES ',
+                    details: '2015-09-10'
+                },
+                {
+                    id: 2,
+                    name: 'CARQUEFOU ',
+                    description: '24 ROUTE DE PARIS 44470 CARQUEFOU ',
+                    date: '2015-10-02'
+                },
+                {
+                    id: 3,
+                    name: 'SAINT-HERBLAIN ',
+                    description: '331 route de Vannes 44800 ST-HERBLAIN ',
+                    date: '2016-03-11'
+                }
+            ];
+
+            // Set the collection of events
+            this.locations = locations;
+
+            // or push them on separately
+            // for (var i in events) {
+            //   this.events.push(events[i]);
+            // }
+        },
+
+        // Adds an event to the existing events array
+        searchLocations: function() {
+            this.fetchLocations();
+        },
+
+        deleteEvent: function(index) {
+            if(confirm("Are you sure you want to delete this event?")) {
+                // $remove is a Vue convenience method similar to splice
+                this.events.splice(index, 1);
+            }
+        }
+    }
+});
